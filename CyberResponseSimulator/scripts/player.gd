9b3dae46616d28e4766943b9b76f74d833f50ba8
TheBigBarry <165207234+TheBigBarry@users.noreply.github.com>
2026-09-02 02:10:05 -0400

    Se quito el movimiento de saltar

--- CyberResponseSimulator/scripts/player.gd
+++ CyberResponseSimulator/scripts/player.gd
@@ -1,7 +1,6 @@
 extends CharacterBody3D
 
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
 const SENSITIVITY = 0.003
 
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -34,9 +33,6 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity.y -= gravity * delta
-
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
 
 	var input_dir := Input.get_vector("move_left", "move_right", "move_forward", "move_back")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
